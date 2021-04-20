fix padding_value caused gradient error

neural.py
@@ -62,7 +62,7 @@ class Decoder(nn.Module):
 
         sent_packed = nn.utils.rnn.pack_padded_sequence(sent, sent_len_sort.cpu())
         outs, (h, c) = self.lstm(sent_packed, (init_h, init_c))
-        outs, _ = nn.utils.rnn.pad_packed_sequence(outs, padding_value=-1e9)
+        outs, _ = nn.utils.rnn.pad_packed_sequence(outs, padding_value=0.0)
 
         outs = outs.index_select(1, idx_reverse)
 

train.py
@@ -6,7 +6,7 @@ from torch import nn, optim
 from torch.utils.data import DataLoader
 from data import ZHIHU_dataset
 from neural import Encoder, Decoder, Seq2Seq
-from tools import *
+from tools import tools_get_logger, tools_get_tensorboard_writer
 
 def setup_seed(seed):
     print(f'setup seed {seed}')
@@ -19,7 +19,7 @@ def setup_seed(seed):
 setup_seed(667)
 device = torch.device('cuda:0')
 
-dataset = ZHIHU_dataset(path='../data/zhihu.txt', topic_num_limit=100, essay_vocab_size=500,
+dataset = ZHIHU_dataset(path='../data/zhihu.txt', topic_num_limit=100, essay_vocab_size=50000,
                         topic_threshold=4, topic_padding_num=5, essay_padding_len=100)
 dataset_loader = DataLoader(dataset, batch_size=64, shuffle=True)
 
@@ -37,6 +37,7 @@ def to_gpu(*params, device=torch.device('cpu')):
 
 def train():
     seq2seq.train()
+    loss_mean = 0.0
     with tqdm(total=len(dataset_loader), desc='train') as pbar:
         for topic, topic_len, essay, essay_len in dataset_loader:
             temp_sos = torch.full([essay.size(0), 1], dataset.essay2idx['<sos>'])
@@ -58,13 +59,18 @@ def train():
             optimizer.step()
 
             pbar.set_postfix_str(f"{loss.item():.4f}")
+            loss_mean += loss.item()
             pbar.update(1)
+    return loss_mean / len(dataset_loader)
 
 
 
 if __name__ == '__main__':
+    writer = tools_get_tensorboard_writer()
     for ep in range(100):
-        train()
+        train_loss = train()
+        writer.add_scalar('Loss/train', train_loss, ep)
+
 
 
 