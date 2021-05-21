29e952101960b7aad64ad4ef819acc5ce1c7f95c
raibows <raibows@hotmail.com>
2021-05-21 16:43:42 +0800

    tag 5/21 only consider memory, no residual

--- knowledge.py
+++ knowledge.py
@@ -7,34 +7,23 @@ from transformer import MultiHeadAttentionLayer
 class Knowledge_v3(nn.Module):
     def __init__(self, embed_size, dec_hid_size, enc_out_size, device):
         super(Knowledge_v3, self).__init__()
-        self.enc_attention = Attention(enc_out_size, dec_hid_size)
-        # self.mem_attention = Attention(embed_size, enc_out_size)
-        self.mem_attention = MultiHeadAttentionLayer(enc_out_size, embed_size, 4, 0.5, device)
-        self.res_mem_attention = MultiHeadAttentionLayer(dec_hid_size, embed_size, 4, 0.5, device)
-        self.attn_mem_layer_norm = nn.LayerNorm(embed_size)
-        self.step_embeddings = None
-        self.dropout = nn.Dropout(0.5)
+        self.mem_attention = MultiHeadAttentionLayer(dec_hid_size, embed_size, 4, 0.5, device)
+        self.knowledge = None
+        self.output_size = embed_size
 
-    def forward(self, dec_hidden, topic_enc_outs, topic_mask, mem_mask):
+    def forward(self, dec_hidden, mem_mask):
         # dec_hidden [batch, 1, dec_hidden_size]
         # topic_enc_outs [batch, topic_num, embed_size]
-        energy_t = self.enc_attention.forward(topic_enc_outs, dec_hidden, topic_mask) #[batch, topic_len, 1]
-        attn_t = topic_enc_outs.permute(0, 2, 1) @ energy_t #[batch, enc_out_size, 1]
-        attn_mem, _ = self.mem_attention.forward(attn_t.permute(0, 2, 1), self.step_embeddings, self.step_embeddings, mask=mem_mask)
-        attn_res_mem, _ = self.res_mem_attention.forward(dec_hidden, self.step_embeddings, self.step_embeddings, mask=mem_mask)
+        attn_mem, _ = self.mem_attention.forward(dec_hidden, self.knowledge, self.knowledge, mem_mask)
 
-        attn_mem = attn_res_mem + self.dropout(attn_mem)
-        attn_mem = self.attn_mem_layer_norm.forward(attn_mem)
+        return attn_mem # [batch, 1, out_size]
 
+    def clear_knowledge(self):
+        self.knowledge = None
 
-        return attn_mem # [batch, 1, embed_size + embed_size]
-
-    def clear_step_embeddings(self):
-        self.step_embeddings = None
-
-    def init_step_embeddings(self, mem_embeddings):
+    def init_knowledge(self, mem_embeddings):
         # [batch, max_mem_num, embed_size]
-        self.step_embeddings = mem_embeddings.detach()
+        self.knowledge = mem_embeddings.detach()
 
 class KnowledgeSeq2Seq(nn.Module):
     def __init__(self, vocab_size, embed_size, pretrained_wv_path, device, mask_idx):
@@ -42,9 +31,9 @@ class KnowledgeSeq2Seq(nn.Module):
         self.encoder = Encoder_LSTM(embed_size, layer_num=1, hidden_size=512,
                                     is_bid=True, is_cat=False, batch_first=True)
         self.decoder_hidden_size = self.encoder.output_size
-        self.feed_to_decoder_size = embed_size + embed_size
-        self.decoder = nn.LSTM(self.feed_to_decoder_size, self.decoder_hidden_size, num_layers=1, batch_first=True)
         self.knowledge = Knowledge_v3(embed_size, self.decoder_hidden_size, self.encoder.output_size, device)
+        self.feed_to_decoder_size = embed_size + self.knowledge.output_size
+        self.decoder = nn.LSTM(self.feed_to_decoder_size, self.decoder_hidden_size, num_layers=1, batch_first=True)
         self.fc_cat = nn.Linear(self.decoder_hidden_size + embed_size, vocab_size)
         self.fc_hidden = nn.Linear(self.decoder_hidden_size, vocab_size)
         self.gate = nn.Linear(self.decoder_hidden_size+embed_size, vocab_size)
@@ -64,14 +53,14 @@ class KnowledgeSeq2Seq(nn.Module):
         if token.dim() == 1:
             token = token.unsqueeze(1)
         embeddings = self.embedding_layer.forward(token)
-        return self.dropout(embeddings)
+        return embeddings
 
-    def before_feed_to_decoder(self, now_input, dec_hidden, enc_outs, topic_mask, mem_mask):
+    def before_feed_to_decoder(self, now_input, dec_hidden, mem_mask):
         # now_input [batch, 1]
         # dec_hidden [1, batch, dec_size]
 
         now_embed = self.forward_only_embedding_layer(now_input)
-        attn_mem = self.knowledge.forward(dec_hidden.permute(1, 0, 2), enc_outs, topic_mask, mem_mask)
+        attn_mem = self.knowledge.forward(dec_hidden.permute(1, 0, 2), mem_mask)
 
         feeds = torch.cat([now_embed, attn_mem], dim=2)
 
@@ -83,8 +72,8 @@ class KnowledgeSeq2Seq(nn.Module):
         :param attn_mem: [batch, embed_size]
         :return: logits [batch, vocab_size]
         """
-        cat = torch.cat([decoder_outs, attn_mem], dim=-1)
         logits_hidden = self.fc_hidden.forward(self.dropout(decoder_outs))
+        cat = torch.cat([decoder_outs, attn_mem], dim=-1)
         logits_cat = self.fc_cat.forward(self.dropout(cat))
         gate = torch.sigmoid(self.gate.forward(cat))
         logits = gate * logits_cat + (1.0 - gate) * logits_hidden
@@ -98,14 +87,13 @@ class KnowledgeSeq2Seq(nn.Module):
         enc_outs, (h, c) = self.encoder.forward(topic_embeddings, topic_len) #[batch, len, embed_size]
         mem_embeddings = self.forward_only_embedding_layer(mems)
         mem_mask = self.make_src_mask(mems)
-        self.knowledge.init_step_embeddings(mem_embeddings.detach())
+        self.knowledge.init_knowledge(mem_embeddings.detach())
         decoder_outputs = torch.zeros([batch, essay_pad_len, self.vocab_size], device=self.device)
 
         if teacher_force:
             for i in range(essay_pad_len):
                 now_input = essay_input[:, i].unsqueeze(1)
-                feeds, attn_mem = self.before_feed_to_decoder(now_input, h, enc_outs,
-                                                              topic_mask, mem_mask)
+                feeds, attn_mem = self.before_feed_to_decoder(now_input, h, mem_mask)
                 outs, (h, c) = self.decoder.forward(feeds, (h, c))
                 logits = self.get_logits(outs.squeeze(), attn_mem)
                 decoder_outputs[:, i, :] = logits
@@ -115,13 +103,12 @@ class KnowledgeSeq2Seq(nn.Module):
             logits[torch.arange(batch), sos_pos] = 1.0
             for i in range(essay_pad_len):
                 now_input = logits.argmax(dim=1).unsqueeze(1)
-                feeds, attn_mem = self.before_feed_to_decoder(now_input, h, enc_outs,
-                                                              topic_mask, mem_mask)
+                feeds, attn_mem = self.before_feed_to_decoder(now_input, h, mem_mask)
                 outs, (h, c) = self.decoder.forward(feeds, (h, c))
                 logits = self.get_logits(outs.squeeze(), attn_mem)
                 decoder_outputs[:, i, :] = logits
 
-        self.knowledge.clear_step_embeddings()
+        self.knowledge.clear_knowledge()
         return decoder_outputs
 
     def make_src_mask(self, src):
